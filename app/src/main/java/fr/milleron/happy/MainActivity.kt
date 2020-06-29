471338b5abcd1c38a4fe30b31ba13e7d1b730525
cat <anne-catherine@milleron.fr>
2020-06-29 16:28:23 +0900

    Cleaning MainActivity Adding sentences in French and in English

--- app/src/main/java/fr/milleron/happy/MainActivity.kt
+++ app/src/main/java/fr/milleron/happy/MainActivity.kt
@@ -1,13 +1,9 @@
 package fr.milleron.happy
 
-import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.AttributeSet
-import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlin.random.Random
 
